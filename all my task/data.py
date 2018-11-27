c96c98849103d4edf21ab2c3d72da25836009ed6
Tanya <tanya.24.01.2001@gmail.com>
2018-11-27 14:11:40 +0200

    data

---
+++ all my task/data.py
@@ -0,0 +1,11 @@
+dataset = \
+{
+    "person_1":{
+        "pasport":"EM345643",
+        "country":{"Country_1": {"food_1","food_2","food_3"},
+                "Country_2": {"food_1","food_2","food_3"}
+    }
+    }
+
+}
+
